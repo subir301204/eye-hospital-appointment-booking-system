c984b9506857d4dbea2206e39ae6e7b8ec69433f
subir301204 <thunderalpha509@gmail.com>
2025-12-06 20:58:25 +0530

    update the App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import BookingForm from "./components/BookingForm";
 import BookingStatus from "./components/BookingStatus";
+import CancelAppointment from "./components/CancelAppointment";
 import Feedback from "./components/Feedback";
-import CancelAppointment from "./components/CancelAppointment"; // <-- add this
 
 function App() {
   return (
-    <div className="app-container">
-      <h1>Eye Hospital Appointment System</h1>
-      {/* Main appointment booking */}
+    <div>
+      <h1 style={{ textAlign: "center", marginTop: "20px" }}>
+        Eye Hospital Appointment System
+      </h1>
+
       <BookingForm />
-      {/* Status checker */}
+
       <BookingStatus />
-      {/* Cancel appointment */}
-      <CancelAppointment /> {/* <-- add this */}
-      {/* Feedback */}
+
+      <CancelAppointment />
+
       <Feedback />
     </div>
   );
